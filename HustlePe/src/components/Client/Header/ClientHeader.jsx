c716ca950ac25c3fe17e0e4caa653210dd1c7891
Niketiiitd <niket22320@iiitd.ac.in>
2024-09-05 06:32:32 +0530

    Update ClientHeader.jsx

--- HustlePe/src/components/Client/Header/ClientHeader.jsx
+++ HustlePe/src/components/Client/Header/ClientHeader.jsx
@@ -1,146 +1,63 @@
-import { Button, Input, Navbar, NavbarContent, NavbarItem } from "@nextui-org/react";
+import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Navbar, NavbarContent, NavbarItem, User } from "@nextui-org/react";
 import React from "react";
-import { NavLink, useNavigate } from 'react-router-dom';
-import { Activity, ChevronDown, Flash, Lock, Scale, Server, TagUser } from "../../../assets/Icons.jsx";
+import { NavLink } from "react-router-dom";
 
-export default function Header() { // Corrected component name from Home to Header
-  const navigate = useNavigate();
-  const icons = {
-    chevron: <ChevronDown fill="currentColor" size={16} />,
-    scale: <Scale className="text-warning" fill="currentColor" size={30} />,
-    lock: <Lock className="text-success" fill="currentColor" size={30} />,
-    activity: <Activity className="text-secondary" fill="currentColor" size={30} />,
-    flash: <Flash className="text-primary" fill="currentColor" size={30} />,
-    server: <Server className="text-success" fill="currentColor" size={30} />,
-    user: <TagUser className="text-danger" fill="currentColor" size={30} />,
-  };
-  const handleLoginClick = () => {
-    navigate('/login'); // Redirect to the login page
-  };
-  const handleSignupClick = () => {
-    navigate('/Intermediate'); // Redirect to the register page
-  };
-
+export default function ClientHeader() {
   return (
     <header className="shadow sticky z-50 top-0">
-      <Navbar>  
-        <NavbarContent className="hidden sm:flex gap-4" justify="center">
-          {/* <Dropdown>
-            <NavbarItem>
-              <DropdownTrigger>
-                <Button
-                  disableRipple
-                  className="p-0 bg-transparent data-[hover=true]:bg-transparent"
-                  endContent={icons.chevron}
-                  radius="sm"
-                  variant="light"
-                >
-                  Features
-                </Button>
-              </DropdownTrigger>
-            </NavbarItem>
-            <DropdownMenu
-              aria-label="ACME features"
-              className="w-[340px]"
-              itemClasses={{
-                base: "gap-4",
-              }}
-            >
-              <DropdownItem
-                key="autoscaling"
-                description="ACME scales apps to meet user demand, automagically, based on load."
-                startContent={icons.scale}
-              >
-                <NavLink to="/autoscaling" className={({ isActive }) =>
-                  `border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-gray-700"}`
-                }>
-                  Autoscaling
-                </NavLink>
-              </DropdownItem>
-              <DropdownItem
-                key="usage_metrics"
-                description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
-                startContent={icons.activity}
-              >
-                <NavLink to="/usage_metrics" className={({ isActive }) =>
-                  `border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-gray-700"}`
-                }>
-                  Usage Metrics
-                </NavLink>
-              </DropdownItem>
-              <DropdownItem
-                key="production_ready"
-                description="ACME runs on ACME, join us and others serving requests at web scale."
-                startContent={icons.flash}
-              >
-                <NavLink to="/production_ready" className={({ isActive }) =>
-                  `border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-gray-700"}`
-                }>
-                  Production Ready
-                </NavLink>
-              </DropdownItem>
-              <DropdownItem
-                key="99_uptime"
-                description="Applications stay on the grid with high availability and high uptime guarantees."
-                startContent={icons.server}
-              >
-                <NavLink to="/99_uptime" className={({ isActive }) =>
-                  `border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-gray-700"}`
-                }>
-                  +99% Uptime
-                </NavLink>
-              </DropdownItem>
-              <DropdownItem
-                key="supreme_support"
-                description="Overcome any challenge with a supporting team ready to respond."
-                startContent={icons.user}
-              >
-                <NavLink to="/supreme_support" className={({ isActive }) =>
-                  `border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-gray-700"}`
-                }>
-                  +Supreme Support
-                </NavLink>
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown> */}
+      <Navbar>
+        <NavbarContent>
           <NavbarItem>
-            <NavLink to="/JobPosting" className={({ isActive }) =>
+            <NavLink to="/client/JobPosting" className={({ isActive }) =>
               `border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-gray-700"}`
             }>
               Job Posting
             </NavLink>
           </NavbarItem>
           <NavbarItem>
-            <NavLink to="/Messages" className={({ isActive }) =>
+            <NavLink to="/client/Messages" className={({ isActive }) =>
               `border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-gray-700"}`
             }>
               Messages
             </NavLink>
           </NavbarItem>
         </NavbarContent>
-        <NavbarContent justify="end" className="items-center">
-          <Input
-            classNames={{
-              base: "max-w-full sm:max-w-[10rem] h-10",
-              mainWrapper: "h-full",
-              input: "text-small",
-              inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-            }}
-            placeholder="Type to search..."
-            size="sm"
-            // startContent={<SearchIcon size={18} />}
-            type="search"
-          />
-          <NavbarItem className="hidden lg:flex">
-          <Button onClick={handleLoginClick}>
-              Login
-            </Button>
-          </NavbarItem>
-          <NavbarItem>
-          <Button onClick={handleSignupClick}>
-              SignUp
-            </Button>
-          </NavbarItem>
+        <NavbarContent className="ml-auto"> {/* This will push the profile dropdown to the right */}
+          <Dropdown placement="bottom-start">
+            <DropdownTrigger>
+              <User
+                as="button"
+                avatarProps={{
+                  isBordered: true,
+                  src: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
+                }}
+                className="transition-transform"
+                description="@tonyreichert"
+                name="Tony Reichert"
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="User Actions" variant="flat">
+              <DropdownItem key="profile" className="h-14 gap-2">
+                <p className="font-bold">Signed in as</p>
+                <p className="font-bold">@tonyreichert</p>
+              </DropdownItem>
+              <DropdownItem key="settings">
+                My Settings
+              </DropdownItem>
+              
+              <DropdownItem key="analytics">
+                Analytics
+              </DropdownItem>
+              
+              <DropdownItem key="configurations">Configurations</DropdownItem>
+              <DropdownItem key="help_and_feedback">
+                Help & Feedback
+              </DropdownItem>
+              <DropdownItem key="logout" color="danger">
+                Log Out
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </NavbarContent>
       </Navbar>
     </header>
